980ad56a6f75e04a71cbafd2d91c9281da5ec409
cnh <cnh@shy.com>
2022-07-19 14:29:42 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author cnhstart
+ * @create 2022-07-19-14:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
